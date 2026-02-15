639075983047057de021d5f6f9bff6d204e4ccfe
Avnoor Kaur <Avnoork2004@gmail.com>
2026-02-15 15:35:30 -0500

    Added The Herd Logo

--- app/src/main/java/com/example/theherd/MainActivity.kt
+++ app/src/main/java/com/example/theherd/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.theherd
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var keepSplash = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { keepSplash }
+
         super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            keepSplash = false
+        }, 3000) // logo stay on for 3 seconds
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
