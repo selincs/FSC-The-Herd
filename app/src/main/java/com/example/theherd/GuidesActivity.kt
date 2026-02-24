ffdc523a3c0def3827e4ac77796ae4bc731683dd
chloeeglouis <chloglouis@gmail.com>
2026-02-23 22:01:59 -0500

    created a GuidesActivity.kt file an a corresponding xml file.

---
+++ app/src/main/java/com/example/theherd/GuidesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.theherd
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GuidesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guides) // connects XML
+    }
+}
