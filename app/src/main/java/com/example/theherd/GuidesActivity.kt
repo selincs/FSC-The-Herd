2b65aefc09e55ff083469ffa59ca44fab02e3ec0
Avnoor Kaur <Avnoork2004@gmail.com>
2026-03-05 20:19:16 -0500

    (guide page) Added settings btn (logout dropdown) option & implementation

--- app/src/main/java/com/example/theherd/GuidesActivity.kt
+++ app/src/main/java/com/example/theherd/GuidesActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageButton
+import androidx.appcompat.widget.PopupMenu
 
 class GuidesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +22,37 @@ class GuidesActivity : AppCompatActivity() {
             println("in createGuideButton onclick listener:")
             val intent = Intent(this, CreateGuideActivity::class.java)
             startActivity(intent)
+        }
+        // Settings button
+        val settingsButton: ImageButton = findViewById(R.id.settingsButton)
+
+        settingsButton.setOnClickListener { view ->
+
+            // Creates popup menu connected to settings button
+            val popupMenu = PopupMenu(this, view)
+            popupMenu.menuInflater.inflate(R.menu.settings_menu, popupMenu.menu)
+
+            // Handles menu clicks
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+
+                    R.id.menu_logout -> {
+
+                        //When settings btn clicked add a way to logout the user
+
+                        // Goes to LoginActivity and clears back stack
+                        val intent = Intent(this, LoginActivity::class.java)
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+            popupMenu.show()
         }
     }
 }
